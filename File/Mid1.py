d2f70940dcfd093ccd6d9170e70b0d1ea7681c5b
KaziSifatAlMaksud <kazi.sifat2013@gmail.com>
2022-11-19 22:41:01 +0600

    git add .

--- File/Mid1.py
+++ File/Mid1.py
@@ -1,8 +1,19 @@
+# f1 = open("data.txt","a")
+# try:
+#     f1.write("to day is sunny day! \n")
+# except:
+#     print("file error")
+# finally:
+#     f1.close()
+#     f1 = open("data.txt")
+#     y = f1.read()
+#     print(y)
+
+d = {1:2,3:"abc"}
 import json
-
-s1 = []
-f1 = open("data.txt","r")
-
-s = f1.read()
-res = json.load(s)
-print(s)
+s = json.dumps(d)
+open("data.txt","w").write(s)
+s2 = open("data.txt").read()
+x = json.loads(s2)
+for keys, i in x.items():
+    print(keys)
