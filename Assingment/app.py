22323a12dfe62c216115436de9addcc2955fd9ae
Kazi Sifat Al Makusd <65750595+KaziSifatAlMaksud@users.noreply.github.com>
2022-12-15 22:06:09 +0600

    ass

--- Assingment/app.py
+++ Assingment/app.py
@@ -10,18 +10,28 @@ def hello_world():
     return 'Hello World!'
 @app.route('/singup',methods=["GET","POST"])
 def singup():
-    if request.method == "post":
-        form_data = request.form()
+    if request.method == "POST":
+        form_data = request.form
         name = form_data["username"]
         email = form_data["email"]
         mobile = form_data["mobile"]
         u_pass = form_data["password"]
         re_pass = form_data["re_password"]
-        d = {"name": name,"email":email,"mobile": mobile,"password":re_pass}
+        d = {"name": name , "email":email ,"mobile":mobile ,"password":re_pass}
         x = mycol.insert_one(d)
     return render_template("singup.html",**locals())
 @app.route('/login',methods=["GET","POST"])
 def login():
+    if request.method == "POST":
+        form_data = request.form
+        email = form_data["email"]
+        passw = form_data["enter_pass"]
+        user_name = mycol.find_one({"email":email})
+        if user_name is None:
+            email_mess = "Email was not register, Please Sign Up"
+        user_passw = mycol.find_one({"password": passw})
+        if user_passw is None:
+            pass_mess = "Password not Correct, Try Again "
     return render_template("login.html",**locals())
 
 if __name__ == '__main__':
