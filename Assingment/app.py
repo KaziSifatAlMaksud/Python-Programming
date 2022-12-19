2524939f2fc4abda68d9743d314c1436e1b09faa
Kazi Sifat Al Makusd <65750595+KaziSifatAlMaksud@users.noreply.github.com>
2022-12-20 01:13:05 +0600

    finish

--- Assingment/app.py
+++ Assingment/app.py
@@ -5,7 +5,24 @@ mycliend = pymongo.MongoClient("mongodb://localhost:27017/assingment")
 mydb = mycliend["usersingup"]
 mycol = mydb["data"]
 
-
+@app.route('/',methods=["GET","POST"])
+def login():
+    if request.method == "POST":
+        form_data = request.form
+        email = form_data["email"]
+        passw = form_data["enter_pass"]
+        user_name = mycol.find_one({"email":email})
+        if user_name is None:
+            email_mess = "Email was not register, Please Sign Up"
+        user_passw = mycol.find_one({"password": passw})
+        if user_passw is None:
+            pass_mess = "Password not Correct, Try Again "
+        if user_passw is not None:
+            profile_name = user_passw["name"]
+            mobile_number = user_passw["mobile"]
+            email_address = user_passw["email"]
+            return render_template("Profile.html", **locals())
+    return render_template("login.html",**locals())
 @app.route('/singup',methods=["GET","POST"])
 def singup():
     if request.method == "POST":
@@ -22,25 +39,7 @@ def singup():
             passmess = "Password is not match"
 
     return render_template("singup.html",**locals())
-@app.route('/',methods=["GET","POST"])
-def login():
-    if request.method == "POST":
-        form_data = request.form
-        email = form_data["email"]
-        passw = form_data["enter_pass"]
-        user_name = mycol.find_one({"email":email})
-        if user_name is None:
-            email_mess = "Email was not register, Please Sign Up"
-        user_passw = mycol.find_one({"password": passw})
 
-        if user_passw is None:
-            pass_mess = "Password not Correct, Try Again "
-        if user_passw is not None:
-            profile_name = user_passw["name"]
-            mobile_number = user_passw["mobile"]
-            email_address = user_passw["email"]
-            return render_template("Profile.html", **locals())
-    return render_template("login.html",**locals())
 @app.route('/profile')
 def profile():
     return render_template("Profile.html",**locals())
