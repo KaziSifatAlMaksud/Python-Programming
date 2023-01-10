54336ebc15bf97ef93d712eec9ca74f20d59fcb4
Kazi Sifat Al Makusd <65750595+KaziSifatAlMaksud@users.noreply.github.com>
2023-01-10 14:39:01 +0600

    asd

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
-@app.route('/form')
+@app.route('/form',methods = ["GET","POST"])
 def form():
-
+    if request.method == "POST":
+        form_data = request.form
+        input = form_data["input"]
+        if input is not None:
+            return input
     return render_template("form.html",**locals())
 
 
